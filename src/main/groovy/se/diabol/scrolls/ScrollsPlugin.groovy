a88a1e33ac84b75fff88a026883d44b7d19e78d4
Robert Dahlström <robert@diabol.se>
2016-10-17 09:38:49 +0200

    Improve docs for plugin interface

--- src/main/groovy/se/diabol/scrolls/ScrollsPlugin.groovy
+++ src/main/groovy/se/diabol/scrolls/ScrollsPlugin.groovy
@@ -5,7 +5,7 @@ package se.diabol.scrolls
  * generate method, providing the oldVersion and the newVersion as input. It's up to the plugin how these parameters are
  * interpreted.
  *
- * In order to list all plugins it's also important to give it a name, hence get getName method.
+ * In order to list all plugins it's also important to give it a name, hence the getName method.
  */
 interface ScrollsPlugin {
     /**
@@ -19,8 +19,8 @@ interface ScrollsPlugin {
      * The actual work method being invoked by Scrolls when it's time to generate a report. It's up to the plugin to
      * determine how to use the oldVersion, newVersion parameters. (E.g. for git it's likely a tag)
      *
-     * @param config The configuration this plugin can use. It's provided from a configuration file and all settings in
-     * it should match the ones the plugin informs about in the #getConfigInfo method.
+     * @param config Configuration for the plugin. It's read from the Scrolls configuration file and the plugin should
+     * probably treat them with some care as it's user input.
      * @param oldVersion The previous version
      * @param newVersion The current version
      * @return A binding that will work in the plugins template
@@ -28,10 +28,9 @@ interface ScrollsPlugin {
     Map generate(Map config, String oldVersion, String newVersion)
 
     /**
-     * This method should provide a detailed description of the available configuration options, keys being the options
-     * and values being the descriptions of said options.
-     * @return Available config options that can customized in the Scrolls config file and provided to the plugin when
-     * #generate() is invoked
+     * This method should provide a detailed description of the available configuration settings for the plugin. Keys
+     * being the options and values being the descriptions of said options.
+     * @return Config options that can be customized in the Scrolls config file
      */
     Map getConfigInfo()
 }
