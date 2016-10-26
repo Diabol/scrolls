8d3b0f014420e2090422dc5bf52875203d3c1f57
Robert Dahlström <robert@diabol.se>
2016-10-26 11:44:12 +0200

    Avoid ; in groovy

--- src/main/groovy/se/diabol/scrolls/engine/ScrollsGenerator.groovy
+++ src/main/groovy/se/diabol/scrolls/engine/ScrollsGenerator.groovy
@@ -8,7 +8,7 @@ import freemarker.template.*
 
 import java.nio.charset.StandardCharsets
 import java.nio.file.Files
-import java.nio.file.Paths;
+import java.nio.file.Paths
 
 class ScrollsGenerator {
 
